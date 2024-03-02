a59cc65d469a753cfea7a933325bfca3e7a53ed6
Danilo Sena <danilo_sena@hotmail.com>
2024-03-01 21:39:11 -0300

    inclusao de funcionalidade cadastro de novos usuarios

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+class Config:
+    # Configurações gerais do Flask
+    SECRET_KEY = 'admin123'
+
+    # Configurações do banco de dados PostgreSQL
+    SQLALCHEMY_DATABASE_URI = 'postgresql://adminpay:admin@localhost/senapay'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
 from app import app
+from app.forms.forms import RegistrationForm
+from app.models import User
+from app.models import db
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Account created successfully!', 'success')
+        return redirect(url_for('index'))
+    return render_template('register.html', title='Register', form=form)
